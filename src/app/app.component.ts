eada79b7c8a40af237bc5d0780bdda92a6e48f38
Gulzat Karimova and Manasa Vesala <gulzat@>
2019-01-23 15:01:36 -0800

    Added edit button

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Task } from './models/task.model';
+
+@Component({
+  selector: 'crazy-app-selector',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  tasks: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course',3),
+    new Task('Begin brainstorming possible JavaScript group projects',2),
+    new Task('Add README file to last few Angular repos on GitHub',1)
+  ];
+  priorityColor(currentTask){
+    if (currentTask.priority === 3){
+     return "bg-danger";
+   } else if (currentTask.priority === 2) {
+     return  "bg-warning";
+   } else {
+     return "bg-info";
+   }
+  }
+
+   selectedTask = null;
+
+   finishedEditing() {
+   this.selectedTask = null;
+ }
+
+   editTask(clickedTask) {
+     this.selectedTask = clickedTask;
+   }
+
+
+
+}
