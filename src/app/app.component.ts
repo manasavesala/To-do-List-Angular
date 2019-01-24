555344a226e6bb372c5d11b17a05bca280ca6d4f
Kaveh Saleminejad and Manasa Vesala <manasa@>
2019-01-24 11:18:11 -0800

    Added more components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,30 +12,23 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  tasks: Task[] = [
+   selectedTask = null;
+   
+   masterTaskList: Task[] = [
     new Task('Finish weekend Angular homework for Epicodus course',3),
     new Task('Begin brainstorming possible JavaScript group projects',2),
     new Task('Add README file to last few Angular repos on GitHub',1)
   ];
-  priorityColor(currentTask){
-    if (currentTask.priority === 3){
-     return "bg-danger";
-   } else if (currentTask.priority === 2) {
-     return  "bg-warning";
-   } else {
-     return "bg-info";
-   }
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
   }
-
-   selectedTask = null;
-
-   finishedEditing() {
+  finishedEditing() {
    this.selectedTask = null;
  }
 
-   editTask(clickedTask) {
-     this.selectedTask = clickedTask;
-   }
+ addTask(newTask: Task) {
+  this.masterTaskList.push(newTask);
+}
 
 
 
---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,35 @@
+import { Task } from '../models/task.model';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent{
+
+  constructor() { }
+  filterByCompleteness: string = "incompleteTasks";
+  @Input() childTaskList: Task[];
+  @Output() clickSender = new EventEmitter();
+
+  editButtonClicked(taskToEdit: Task) {
+    this.clickSender.emit(taskToEdit);
+  }
+
+  priorityColor(currentTask){
+    if (currentTask.priority === 3){
+     return "bg-danger";
+   } else if (currentTask.priority === 2) {
+     return  "bg-warning";
+   } else {
+     return "bg-info";
+   }
+  }
+  onChange(optionFromMenu) {
+    this.filterByCompleteness = optionFromMenu;
+  }
+  toggleDone(clickedTask: Task, setCompleteness: boolean) {
+    clickedTask.done = setCompleteness;
+  }
+}
